5184052166a8d7506736c7781fb82c35f620a3dd
Your Name <you@example.com>
2022-02-28 14:25:50 +0300

    Bot

--- main.py
+++ main.py
@@ -1,44 +1,27 @@
 import telebot, wikipedia, re
 # Создаем экземпляр бота
 bot = telebot.TeleBot('5248690115:AAG8LQrB0GIQh7MKOF_JUItcDF5qszINRUU')
-# Устанавливаем русский язык в Wikipedia
-wikipedia.set_lang("ru")
-# Чистим текст статьи в Wikipedia и ограничиваем его тысячей символов
-def getwiki(s):
-    try:
-        ny = wikipedia.page(s)
-        # Получаем первую тысячу символов
-        wikitext=ny.content[:1000]
-        # Разделяем по точкам
-        wikimas=wikitext.split('.')
-        # Отбрасываем всЕ после последней точки
-        wikimas = wikimas[:-1]
-        # Создаем пустую переменную для текста
-        wikitext2 = ''
-        # Проходимся по строкам, где нет знаков «равно» (то есть все, кроме заголовков)
-        for x in wikimas:
-            if not('==' in x):
-                    # Если в строке осталось больше трех символов, добавляем ее к нашей переменной и возвращаем утерянные при разделении строк точки на место
-                if(len((x.strip()))>3):
-                   wikitext2=wikitext2+x+'.'
-            else:
-                break
-        # Теперь при помощи регулярных выражений убираем разметку
-        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
-        # Возвращаем текстовую строку
-        return wikitext2
-    # Обрабатываем исключение, которое мог вернуть модуль wikipedia при запросе
-    except Exception as e:
-        return 'В энциклопедии нет информации об этом'
-# Функция, обрабатывающая команду /start
-@bot.message_handler(commands=["start"])
-def start(m, res=False):
-    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
-# Получение сообщений от юзера
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-    bot.send_message(message.chat.id, getwiki(message.text))
-# Запускаем бота
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет!')
+
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Я умею направлять людей. Напиши "вуз" и я скину ссылку на сайт твоего ВУЗа')
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    if message.text.lower() == "вуз":
+        bot.send_message(message.chat.id, 'Тебе сюда – https://mtuci.ru/')
+    if message.text.lower() == 'как тебя зовут?':
+        bot.send_message(message.chat.id, 'Оливия!')
+    if message.text.lower() == 'как у тебя дела?':
+        bot.send_message(message.chat.id, 'Отлично! У тебя?')
+    if message.text.lower() == 'тоже':
+        bot.send_message(message.chat.id, '')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Пока!')
+
+
+
 bot.polling(none_stop=True, interval=0)
